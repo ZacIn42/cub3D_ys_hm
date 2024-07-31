bde54f69b66e322efdc0747572f972d374e17735
Haruto Miyazaki <hmiyazak@c3r3s8.42tokyo.jp>
2024-07-31 19:39:00 +0900

    init

---
+++ cub.h
@@ -0,0 +1,14 @@
+#ifndef CUB_H
+# define CUB_H
+
+# include <mlx.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/time.h>
+
+#endif
