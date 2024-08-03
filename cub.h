481c8c554e301efb5642a60d749a4ede3683101b
Yusuke Sato <yususato@c3r2s3.42tokyo.jp>
2024-08-03 21:24:14 +0900

    aaa

--- cub.h
+++ cub.h
@@ -1,14 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yususato <yususato@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 19:30:39 by yususato          #+#    #+#             */
+/*   Updated: 2024/08/03 19:30:42 by yususato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB_H
 # define CUB_H
 
 # include "./mlx_linux/mlx.h"
+# include <math.h>
 # include <unistd.h>
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
 # include <sys/types.h>
 # include <sys/stat.h>
+# include <sys/time.h>
 # include <fcntl.h>
 # include <errno.h>
-# include <sys/time.h>
+# include <limits.h>
+# include <string.h>
+
+# define WIN_HEIGHT (2048)
+# define WIN_WIDTH (2048)
+// # define BLOCK_HEIGHT (1024)
+// # define BLOCK_WIDTH (1024)
+
+typedef struct s_vec {
+	double	x;
+	double	y;
+}	t_vec;
+
+typedef enum e_direction {
+	NORTH,
+	EAST,
+	SOUTH,
+	WEST,
+}	t_dir;
+
+typedef struct s_user {
+	t_vec	pos;
+	t_dir	dir;
+}	t_user;
+
+typedef struct s_img {
+	void	*img;
+	char	*addr;
+	int		width;
+	int		height;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_img;
+
+typedef struct s_field {
+	char			**map;
+	t_user			user;
+	char			texture_paths[4][PATH_MAX];
+	t_img			textures[4];
+	unsigned int	c_color;
+	unsigned int	f_color;
+}	t_field;
+
+typedef struct s_cub {
+	void	*mlx;
+	void	*window;
+	t_img	img;
+	t_field	*field;
+}	t_cub;
+
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+enum {
+	LOOK_LEFT_KEY = 65361,
+	LOOK_RIGHT_KEY = 65363,
+	MOVE_FORWARD_KEY = 119,
+	MOVE_RIGHT_KEY = 100,
+	MOVE_BACK_KEY = 115,
+	MOVE_LEFT_KEY = 97,
+	ESC_KEY = 65307,
+};
+
+void	vec_init(t_vec *vec, double x, double y);
+void	vec_add(t_vec *ans, t_vec *lhs, t_vec *rhs);
+void	vec_sub(t_vec *ans, t_vec *lhs, t_vec *rhs);
+// int		draw_wall(t_cub *mlx, t_user *user, t_vec *w_start, t_vec *w_end);
+t_vec	*cast_ray_alloc(const t_vec *pos, double theta, const char **map);
+int		set_mlx_hooks(t_cub *cub);
+int		move(int key_code, t_user *user);
+bool	is_look_key(int key_code);
+int		look_around(int key_code, t_user *user);
+int		close_window_esc(int key_code, t_cub *mlx);
 
 #endif
---
+++ ray_cast.c
@@ -0,0 +1,184 @@
+#include <mlx.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+#define TEX_WIDTH 64
+#define TEX_HEIGHT 64
+
+typedef struct  s_mlx {
+    void        *mlx;
+    void        *win;
+    void        *img;
+    int         *data;
+    int         bpp;
+    int         size_l;
+    int         endian;
+}               t_mlx;
+
+typedef struct  s_player {
+    double      posX;
+    double      posY;
+    double      dirX;
+    double      dirY;
+    double      planeX;
+    double      planeY;
+}               t_player;
+
+int worldMap[MAP_WIDTH][MAP_HEIGHT] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+void    raycasting(t_mlx *mlx, t_player *player)
+{
+    for (int x = 0; x < WIN_WIDTH; x++)
+    {
+        // カメラプレーン内のX座標を計算
+        double cameraX = 2 * x / (double)WIN_WIDTH - 1;
+        double rayDirX = player->dirX + player->planeX * cameraX;
+        double rayDirY = player->dirY + player->planeY * cameraX;
+
+        // 現在のマップ位置
+        int mapX = (int)player->posX;
+        int mapY = (int)player->posY;
+
+        // 距離を測定するための変数
+        double sideDistX;
+        double sideDistY;
+
+        // 一つのXまたはY方向に進む距離
+        double deltaDistX = fabs(1 / rayDirX);
+        double deltaDistY = fabs(1 / rayDirY);
+        double perpWallDist;
+
+        // ステップの方向と初期サイドディスタンス
+        int stepX;
+        int stepY;
+
+        int hit = 0; // 壁にぶつかったか
+        int side; // XかYか
+
+        if (rayDirX < 0)
+        {
+            stepX = -1;
+            sideDistX = (player->posX - mapX) * deltaDistX;
+        }
+        else
+        {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - player->posX) * deltaDistX;
+        }
+        if (rayDirY < 0)
+        {
+            stepY = -1;
+            sideDistY = (player->posY - mapY) * deltaDistY;
+        }
+        else
+        {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - player->posY) * deltaDistY;
+        }
+
+        // DDAアルゴリズムを使って壁にぶつかるまで進む
+        while (hit == 0)
+        {
+            if (sideDistX < sideDistY)
+            {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            }
+            else
+            {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            if (worldMap[mapX][mapY] > 0) hit = 1;
+        }
+
+        // 壁までの垂直距離を計算
+        if (side == 0) perpWallDist = (mapX - player->posX + (1 - stepX) / 2) / rayDirX;
+        else perpWallDist = (mapY - player->posY + (1 - stepY) / 2) / rayDirY;
+
+        // 壁の高さを計算
+        int lineHeight = (int)(WIN_HEIGHT / perpWallDist);
+
+        // 描画開始と終了位置を計算
+        int drawStart = -lineHeight / 2 + WIN_HEIGHT / 2;
+        if (drawStart < 0) drawStart = 0;
+        int drawEnd = lineHeight / 2 + WIN_HEIGHT / 2;
+        if (drawEnd >= WIN_HEIGHT) drawEnd = WIN_HEIGHT - 1;
+
+        // 壁の色を決定
+        int color;
+        if (side == 1) color = 0xAA0000; // Y方向の壁
+        else color = 0x00AA00; // X方向の壁
+
+        // 垂直線を描画
+        for (int y = drawStart; y < drawEnd; y++)
+        {
+            mlx->data[y * WIN_WIDTH + x] = color;
+        }
+    }
+    // イメージをウィンドウに表示
+    mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+}
+
+int main()
+{
+    t_mlx mlx;
+    t_player player;
+
+    // MiniLibXの初期化
+    mlx.mlx = mlx_init();
+    mlx.win = mlx_new_window(mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "Raycasting");
+    mlx.img = mlx_new_image(mlx.mlx, WIN_WIDTH, WIN_HEIGHT);
+    mlx.data = (int *)mlx_get_data_addr(mlx.img, &mlx.bpp, &mlx.size_l, &mlx.endian);
+
+    // プレイヤーの初期位置と方向
+    player.posX = 22.0;
+    player.posY = 12.0;
+    player.dirX = -1.0;
+    player.dirY = 0.0;
+    player.planeX = 0.0;
+    player.planeY = 0.66;
+
+    // メインループ
+    while (1)
+    {
+        // レイキャスティングでシーンを描画
+        raycasting(&mlx, &player);
+
+        // イベントループ
+        mlx_loop(mlx.mlx);
+    }
+    return 0;
+}
