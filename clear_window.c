ed9e12a9a3f82555aa2ab7a16aeb8920f6505e67
Yusuke Sato <yususato@c3r2s3.42tokyo.jp>
2024-08-03 17:45:14 +0900

    clear

--- clear_window.c
+++ clear_window.c
@@ -1,47 +1,66 @@
 #include "cub.h"
 
+typedef struct s_data {
+    void    *mlx;
+    void    *win;
+    void    *img;
+    char    *addr;
+    int     bits;
+    int     line;
+    int     endian;
+    int     img_width;
+    int     img_height;
+} t_data;
+
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line + x * (data->bits / 8));
+    *(unsigned int *)dst = color;
+}
+
+int key_press(int keycode, t_data *data)
+{
+    if (keycode == 126)
+        printf("Up\n");
+    else if (keycode == 125)
+        printf("Down\n");
+    mlx_clear_window(data->mlx, data->win);
+    mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
+    return (0);
+}
+
 int main(void)
 {
-    void    *mlx_ptr;
-    void    *win_ptr;
+    t_data  data;
+    int     x;
+    int     y;
+    int     color1;
+    int     color2;
 
-    // MiniLibX の初期化
-    mlx_ptr = mlx_init();
-    if (mlx_ptr == NULL)
-        return (1);
-
-    // ウィンドウの作成 (幅: 800, 高さ: 600)
-    win_ptr = mlx_new_window(mlx_ptr, 800, 600, "Clear Window Example");
-    if (win_ptr == NULL)
+    x = 0;
+    y = 0;
+    color1 = 0x0000FF;
+    color2 = 0x000000;
+    data.mlx = mlx_init();
+    data.win = mlx_new_window(data.mlx, 800,600,"cub3D");
+    data.img = mlx_new_image(data.mlx, 800, 600);
+    data.addr = mlx_get_data_addr(data.img, &data.bits, &data.line, &data.endian);
+    while (y < 600)
     {
-        mlx_destroy_display(mlx_ptr);
-        free(mlx_ptr);
-        return (1);
-    }
-
-    // 四角形を描画
-    int x_start = 100;
-    int y_start = 100;
-    int width = 200;
-    int height = 150;
-    int color = 0x00FF00;  // 緑色
-
-    for (int x = x_start; x < x_start + width; x++)
-    {
-        for (int y = y_start; y < y_start + height; y++)
+        while (x < 800)
         {
-            mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+            if (y <= 300)
+                my_mlx_pixel_put(&data, x, y, color2);
+            else
+                my_mlx_pixel_put(&data, x, y, color1);
+            x++;
         }
+        x = 0;
+        y++;
     }
-
-    // 少し待機 (ユーザーが四角形を確認できるように)
-    sleep(3);
-
-    // ウィンドウをクリア
-    mlx_clear_window(mlx_ptr, win_ptr);
-
-    // イベントループを開始
-    mlx_loop(mlx_ptr);
-
-    return (0);
+    mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+    mlx_key_hook(data.win, key_press, &data);
+    mlx_loop(data.mlx);
 }
