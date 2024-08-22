8179e01fad53fde1718e28857235693fb7487461
Haruto Miyazaki <hmiyazak@c3r4s8.42tokyo.jp>
2024-08-22 14:59:15 +0900

    imploved wall detection

--- draw_scene/cast_ray.c
+++ draw_scene/cast_ray.c
@@ -6,37 +6,37 @@
 /*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 12:46:06 by hmiyazak          #+#    #+#             */
-/*   Updated: 2024/08/14 10:34:59 by hmiyazak         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:49:44 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-static void	set_next_border(t_vec *current_ray, double theta, t_vec *next_wall);
+static void	set_next_border(t_vec *current_ray, double theta);
 static void	set_next_x_y(t_vec *current_ray, t_vec *next_x_y, double theta);
 static bool	is_next_x(t_vec *current_ray, t_vec *next_x_y, double theta);
-static bool	is_block(char **map, int x, int y);
+static bool	is_block(char **map, const t_vec *pos, t_vec *current_ray);
 
 t_vec	*cast_ray_alloc(const t_vec *pos, double theta, char **map)
 {
 	t_vec	*current_ray;
-	t_vec	next_wall;
+	t_vec	next_block;
 
 	current_ray = (t_vec *)malloc(sizeof(t_vec));
 	if (current_ray == NULL)
 		exit(1);
 	vec_init(current_ray, pos->x, pos->y);
-	vec_init(&next_wall, (int)pos->x, (int)pos->y);
+	vec_init(&next_block, (int)pos->x, (int)pos->y);
 	while (true)
 	{
-		set_next_border(current_ray, theta, &next_wall);
-		if (is_block(map, next_wall.x, next_wall.y))
+		set_next_border(current_ray, theta);
+		if (is_block(map, pos, current_ray))
 			break ;
 	}
 	return (current_ray);
 }
 
-static void	set_next_border(t_vec *current_ray, double theta, t_vec *next_wall)
+static void	set_next_border(t_vec *current_ray, double theta)
 {
 	t_vec	next_x_y;
 	t_vec	dif_x_y;
@@ -45,20 +45,14 @@ static void	set_next_border(t_vec *current_ray, double theta, t_vec *next_wall)
 	vec_init(&dif_x_y, next_x_y.x - current_ray->x, \
 						next_x_y.y - current_ray->y);
 	if (cos(theta) == 0)
+		current_ray->y = next_x_y.y;
+	else if (is_next_x(current_ray, &next_x_y, theta))
 	{
-		next_wall->y = next_x_y.y;
-		current_ray->y = next_wall->y;
-		return ;
-	}
-	if (is_next_x(current_ray, &next_x_y, theta))
-	{
-		next_wall->x = next_x_y.x;
 		current_ray->x = next_x_y.x;
 		current_ray->y += dif_x_y.x * tan(theta);
 	}
 	else
 	{
-		next_wall->y = next_x_y.y;
 		current_ray->y = next_x_y.y;
 		current_ray->x += dif_x_y.y / tan(theta);
 	}
@@ -97,7 +91,18 @@ static bool	is_next_x(t_vec *current_ray, t_vec *next_x_y, double theta)
 		return (false);
 }
 
-static bool	is_block(char **map, int x, int y)
+static bool	is_block(char **map, const t_vec *pos, t_vec *current_ray)
 {
+	int	x;
+	int	y;
+
+	if (current_ray->x == (int)current_ray->x && current_ray->x < pos->x)
+			x = (int)current_ray->x;
+	else
+		x = (int)current_ray->x + 1;
+	if (current_ray->y == (int)current_ray->y && current_ray->y < pos->y)
+			y = (int)current_ray->y;
+	else
+		y = (int)current_ray->y + 1;
 	return (map[y][x] == '1');
 }
