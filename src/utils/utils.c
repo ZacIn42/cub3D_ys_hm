9b185d96de98abe9d3e2eff228bd0ee794f704c0
Haruto Miyazaki <hmiyazak@c3r6s8.42tokyo.jp>
2024-08-24 18:18:55 +0900

    added error handling for write systemcall

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 18:09:11 by hmiyazak          #+#    #+#             */
-/*   Updated: 2024/08/24 18:12:29 by hmiyazak         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:18:06 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ static void	put_error(char *error_message)
 		return ;
 	while (*error_message != '\0')
 	{
-		write(2, error_message, 1);
+		if (write(2, error_message, 1) < 0)
+			return ;
 		error_message += 1;
 	}
 }
