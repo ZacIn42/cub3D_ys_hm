1594563f7555ad76838a89c73aefb83d70c4f3bd
Haruto Miyazaki <hmiyazak@c3r6s8.42tokyo.jp>
2024-08-24 18:59:58 +0900

    modified cub.h

--- src/parse_file/parse_other/get_next_line.c
+++ src/parse_file/parse_other/get_next_line.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yususato <yususato@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 16:29:02 by yususato          #+#    #+#             */
-/*   Updated: 2024/08/23 11:12:57 by yususato         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:58:53 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-char	*line_next(char *buffer, char *line)
+static char	*line_next(char *buffer, char *line)
 {
 	size_t	i;
 	size_t	j;
@@ -40,7 +40,7 @@ char	*line_next(char *buffer, char *line)
 	return (tmp);
 }
 
-char	*output(char *buffer)
+static char	*output(char *buffer)
 {
 	char	*line;
 	size_t	i;
@@ -67,7 +67,7 @@ char	*output(char *buffer)
 	return (line);
 }
 
-char	*unit(char *buffer, char *buf)
+static char	*unit(char *buffer, char *buf)
 {
 	char	*temp;
 
@@ -76,7 +76,7 @@ char	*unit(char *buffer, char *buf)
 	return (temp);
 }
 
-char	*read_file(int fd, char *buffer, int *flag)
+static char	*read_file(int fd, char *buffer, int *flag)
 {
 	int		byte_size;
 	char	*buf;
