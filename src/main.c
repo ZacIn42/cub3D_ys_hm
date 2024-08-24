8521877a5fe1f7ba4842d9c6ad9716343e62c09b
Yusuke Sato <yususato@c3r6s6.42tokyo.jp>
2024-08-24 18:31:51 +0900

    parse_file

--- src/main.c
+++ src/main.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yususato <yususato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 09:05:11 by hmiyazak          #+#    #+#             */
-/*   Updated: 2024/08/24 17:58:49 by hmiyazak         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:29:25 by yususato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+void	check_filename(int argument_count, char *filename)
+{
+	char	*extension;
+
+	if (argument_count != 2)
+		exit(0);
+	extension = strrchr(filename, '.');
+	if (extension == NULL)
+		exit(0);
+	if (strncmp(extension, ".cub\0", 5) != 0)
+		exit(0);
+}
+
 
 int	main(void)
 {
